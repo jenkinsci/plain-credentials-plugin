ce212cc43c52b0f8a7c968855c23277e2657f520
Stephen Connolly <stephen.alan.connolly@gmail.com>
2016-09-23 10:57:45 +0100

    [JENKINS-36432 followup] Add tests of migration and reading plain-text
    - The reading of plain-text from the on-disk XML is for e.g. people using chef/puppet scripts to pre-populate their JENKINS_HOME
    - The reading of plain-text from the CLI create credentials command is an obvious additional use case. This also applies to the REST API for credentials creation, but as the REST API is already tested in the credentials plugin I do not see any value in adding a specific test for that scenario.
    - The migration of legacy data intact is also an obvious requirement

--- src/main/java/org/jenkinsci/plugins/plaincredentials/impl/FileCredentialsImpl.java
+++ src/main/java/org/jenkinsci/plugins/plaincredentials/impl/FileCredentialsImpl.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2013 Jesse Glick, Stephen Connolly and CloudBees, Inc.
+ * Copyright 2013-2016 Jesse Glick, Stephen Connolly and CloudBees, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

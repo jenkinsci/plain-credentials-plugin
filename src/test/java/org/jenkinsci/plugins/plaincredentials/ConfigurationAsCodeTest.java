5672265b5ebb7ca16696699a774adc16e766269f
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-11-16 08:29:57 +0100

    test-case to demonstrate casc-support
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ src/test/java/org/jenkinsci/plugins/plaincredentials/ConfigurationAsCodeTest.java
@@ -0,0 +1,35 @@
+package org.jenkinsci.plugins.plaincredentials;
+
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.SecretBytes;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
+import hudson.security.ACL;
+import io.jenkins.plugins.casc.ConfigurationAsCode;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class ConfigurationAsCodeTest {
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void should_configure_file_credentials() throws Exception {
+        SecretBytes.decrypt(null); // force class loading to get Stapler converter registered
+        ConfigurationAsCode.get().configure(getClass().getResource("ConfigurationAsCode.yaml").toString());
+        final FileCredentials credentials = CredentialsMatchers.firstOrNull(
+                CredentialsProvider.lookupCredentials(FileCredentials.class, j.jenkins, ACL.SYSTEM, (DomainRequirement) null),
+                CredentialsMatchers.withId("secret-file"));
+        Assert.assertNotNull(credentials);
+        Assert.assertEquals("Some secret file", credentials.getDescription());
+        Assert.assertEquals("my-secret-file", credentials.getFileName());
+        Assert.assertEquals("FOO_BAR", IOUtils.toString(credentials.getContent()));
+    }
+}

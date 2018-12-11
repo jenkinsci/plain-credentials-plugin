97d4a627e389d561704ff63245e856c75930069a
Jesse Glick <jglick@cloudbees.com>
2018-12-10 19:09:20 -0500

    Apparently unnecessary workaround.

--- src/test/java/org/jenkinsci/plugins/plaincredentials/ConfigurationAsCodeTest.java
+++ src/test/java/org/jenkinsci/plugins/plaincredentials/ConfigurationAsCodeTest.java
@@ -2,7 +2,6 @@ package org.jenkinsci.plugins.plaincredentials;
 
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
-import com.cloudbees.plugins.credentials.SecretBytes;
 import com.cloudbees.plugins.credentials.domains.DomainRequirement;
 import hudson.security.ACL;
 import io.jenkins.plugins.casc.ConfigurationAsCode;
@@ -22,7 +21,6 @@ public class ConfigurationAsCodeTest {
 
     @Test
     public void should_configure_file_credentials() throws Exception {
-        SecretBytes.decrypt(null); // force class loading to get Stapler converter registered
         ConfigurationAsCode.get().configure(getClass().getResource("ConfigurationAsCode.yaml").toString());
         final FileCredentials credentials = CredentialsMatchers.firstOrNull(
                 CredentialsProvider.lookupCredentials(FileCredentials.class, j.jenkins, ACL.SYSTEM, (DomainRequirement) null),
